dbc8162a3659807b405e1e86d15e6d12075a1f94
hachibu <raymondsohn@gmail.com>
2021-12-28 21:30:57 -0800

    Refactor main.py variable names

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from util import run_trial, run_trials, gen_chart_html, gen_index_html
 titles = []
 
 trials = [100, 1000, 10000, 100000]
-for x, y in enumerate(trials):
-    title = "trial-{}-{}-flips".format(x, y)
+for i, n in enumerate(trials):
+    title = "trial-{}-{}-flips".format(i, n)
     titles.append(title)
-    gen_chart_html(title, run_trial(y))
+    gen_chart_html(title, run_trial(n))
 
 experiments = [(100, 1000), (1000, 1000)]
-for x, (y, z) in enumerate(experiments):
-    title = "experiment-{}-{}-trials-of-{}-flips".format(x, y, z)
+for i, (n, m) in enumerate(experiments):
+    title = "experiment-{}-{}-trials-of-{}-flips".format(i, n, m)
     titles.append(title)
-    gen_chart_html(title, run_trials(y, z))
+    gen_chart_html(title, run_trials(n, m))
 
 gen_index_html(titles)
 
